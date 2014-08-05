0c0a08912575457e57a0ee357956d3cf7e47b3b2
Puneeth Chaganti <punchagan@muse-amuse.in>
2014-08-05 18:41:43 -0400

    Get LED to light up without bit shifts.

--- source/main.s
+++ source/main.s
@@ -6,17 +6,18 @@ _start:
 	ldr r0,=0x20200000
 
 	// Make pin16 an output pin.
-	mov r1,#1
-	lsl r1,#18
-	str r1,[r0,#4]
-	// Why does this not work??
-	// mov r1,#4
-	// str r1,[r0,#6]
+	// mov r1,#1
+	// lsl r1,#18
+	// str r1,[r0,#4]
+
+	// Make pin16 an output pin (without the bit shifts)
+	mov r1,#4
+	str r1,[r0,#5]  // NOTE: big endian, makes it #5, and not #6
 
 	// Turn off the GPIO pin to turn the LED on
 	mov r1,#1
 	lsl r1,#16
-	str r1,[r0,#40]
+	str r1,[r0,#40]  // #40 --> 0x28
 
 loop$:
 	// Loop forever
